e1d12c3e45a92e088abfcc34c1755f5f9a930d50
lagouttiere-crypt <la.gouttiere@mac.com>
2026-03-28 23:14:37 +0100

    mise à jour

--- publier.sh
+++ publier.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Dossier Obsidian
 OBSIDIAN="/Users/matthieugaudeau/Library/Mobile Documents/iCloud~md~obsidian/Documents/Technique Alexander/noyau théorique/Pédagogie-Perceptive"
 
-# Copie depuis Obsidian vers Quarto
 cp "$OBSIDIAN/introduction.md" /Users/matthieugaudeau/Documents/pedagogie-enactive/intro.qmd
 cp "$OBSIDIAN/chapitre-1.md" /Users/matthieugaudeau/Documents/pedagogie-enactive/chapitre-1.qmd
+cp -r "$OBSIDIAN/photos/" /Users/matthieugaudeau/Documents/pedagogie-enactive/images/
 
-# Publie
 quarto render
 git add .
 git commit -m "mise à jour"
